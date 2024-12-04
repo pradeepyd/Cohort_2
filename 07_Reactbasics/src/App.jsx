bbd3d537544a1340e8899f143113062a447f754a
Pradeep yadav <pradeep20020102@gmail.com>
2024-12-04 22:29:32 +0545

    basic todo with array state

--- 07_Reactbasics/src/App.jsx
+++ 07_Reactbasics/src/App.jsx
@@ -1,19 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-  function clickHandler(){
-    setCount(count + 1);
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([
+    {
+      title: "go to gym",
+      description: "at morning 6 AM"
+    },
+    {
+      title: "go to market",
+      description: "at evening 6 PM"
+    },
+    {
+      title: "Study DSA",
+      description: "STUDY at morning 4 AM"
+    }
+  ])
+  function addTodo(){
+    setTodos([...todos,{
+      title :"new todo added",
+      description: "description of new todo "
+    }])
   }
+  
   return (
     <div>
-      <button onClick={clickHandler}>Counter {count} </button>
+      <button onClick={addTodo}>Get a random Todo</button>
+     {/* <CustomButton count={count} setCount={setCount}></CustomButton> */}
+    {todos.map(function(todo){
+      return <Todo title={todo.title} description={todo.description}/>
+    })}
     </div>
   )
+}
+
+function Todo(props){
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+  </div>
+}
+
+function CustomButton(props){
+
+  function clickHandler(){
+    props.setCount(props.count + 1);
+  }
+  return <button onClick={clickHandler}>
+    Counter {props.count}
+    </button>
 }
 
 export default App
