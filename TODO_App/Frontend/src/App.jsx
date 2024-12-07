62b6be0542271541cb2cd50419727123819042a9
Pradeep yadav <pradeep20020102@gmail.com>
2024-12-07 22:29:45 +0545

    useEfect hook was itroduced

--- TODO_App/Frontend/src/App.jsx
+++ TODO_App/Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,15 +8,21 @@ import { Todos } from './Components/Todos'
 function App() {
 const [todos,setTodos] = useState([]);
 
-fetch("http://localhost:3000/todos")
-.then(async function(res){
-const json = await res.json();
-setTodos(json.todos);
-})
+useEffect(() => {
+  setInterval(()=>{
+    fetch("http://localhost:3000/todos")
+    .then(async function(res){
+    const json = await res.json();
+    setTodos(json.todos);
+    })
+  },10000)
+},[])
+  
+
   return (
      <div>
-      <CreateTodo></CreateTodo>
-      <Todos todos={todos}></Todos>
+      <CreateTodo key={todos.id}></CreateTodo>
+      <Todos key={todos.id} todos={todos}></Todos>
      </div>
   )
 }
