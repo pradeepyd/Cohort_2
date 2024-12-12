41cfb8bab7be81e1c6d7fc63e96e03e7a9be67e6
Pradeep yadav <pradeep20020102@gmail.com>
2024-12-12 23:23:56 +0545

    async data call are learned

--- Recoil-Advance/Easy-bits/src/App.jsx
+++ Recoil-Advance/Easy-bits/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
-import { jobAtom, networkAtom ,notificationAtom,messagingAtom, totalCount} from './store/atom'
+import { jobAtom, networkAtom ,notificationAtom,messagingAtom, totalCount} from './atoms'
 
 function App() {
   
