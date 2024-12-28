abc3d1c59fc0b69d1f22f7786b0fd34b7a72affb
Pradeep yadav <pradeep20020102@gmail.com>
2024-12-28 18:09:57 +0545

    crud operation done

--- postgres/src/index.ts
+++ postgres/src/index.ts
@@ -1,23 +1,24 @@
-
 import { Client } from "pg";
 
 const client = new Client({
-    connectionString:"postgresql://test_owner:XJYM3cvti6Pg@ep-still-pine-a5ybfutz.us-east-2.aws.neon.tech/test?sslmode=require"
-})
-
+  connectionString:
+    "postgresql://test_owner:XJYM3cvti6Pg@ep-still-pine-a5ybfutz.us-east-2.aws.neon.tech/test?sslmode=require",
+});
 
 async function createUserTable() {
-    await client.connect();
-    const result = await client.query(`
-        CREATE TABLE users(
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(50) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  await client.connect();
+  const result = await client.query(`
+        CREATE TABLE addresses (
+          id SERIAL PRIMARY KEY,
+          user_id INTEGER NOT NULL,
+          city VARCHAR(100) NOT NULL,
+          country VARCHAR(100) NOT NULL,
+          street VARCHAR(255) NOT NULL,
+          pincode VARCHAR(20),
+          created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+          FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
         );
-    `)
-    console.log(result);
+    `);
+  console.log(result);
 }
-
 createUserTable();
