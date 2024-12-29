171d579dc7cd792e38f77c0d3a5fa69c99fba5c4
Pradeep yadav <pradeep20020102@gmail.com>
2024-12-29 23:59:38 +0545

    serverless backend done by deploying

---
+++ serverless-backend/src/index.ts
@@ -0,0 +1,21 @@
+export interface Env{
+
+}
+
+export default {
+	async fetch(request:Request, env:Env, ctx:ExecutionContext): Promise<Response> {
+		console.log(request.body)
+		console.log(request.headers)
+		console.log(request.method)
+		
+		if(request.method === "GET"){
+			return Response.json({
+				message :"you sent a get request"
+			})
+		} else {
+			return Response.json({
+				message :"you donot send a get request"
+			})
+		}
+	},
+} 
