50b2830887990b95a57014ae3870dd58f0e51e05
Pradeep yadav <pradeep20020102@gmail.com>
2024-12-29 11:52:32 +0545

    updating user done

--- Prisma/src/index.ts
+++ Prisma/src/index.ts
@@ -2,19 +2,43 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function insertUser(username: string, password: string, firstName: string, lastName: string) {
-  const res = await prisma.user.create({
+// async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+//   const res = await prisma.user.create({
+//     data:{
+//         email:username,
+//         password,
+//         firstName,
+//         lastName
+//     },
+//     select:{
+//         id:true,
+//         password:true
+//     }
+//   })
+//   console.log(res)
+// }
+// insertUser("pradeep1@gmail.com","12345","pradeep","yadav");
+
+ interface updateParams {
+  firstName:string,
+  lastName:string
+ }
+
+ async function updateUser(username:string, {
+  firstName,
+  lastName
+}:updateParams){
+  const res = await prisma.user.update({
+    where:{email:username},
     data:{
-        email:username,
-        password,
-        firstName,
-        lastName
-    },
-    select:{
-        id:true,
-        password:true
+      firstName,
+      lastName
     }
   })
   console.log(res)
-}
-insertUser("pradeep1@gmail.com","12345","pradeep","yadav");
+ }
+
+ updateUser("pradeep@gmail.com",{
+  firstName:"pradeeep1111",
+  lastName:"yadavv"
+})
