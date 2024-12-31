d10b8e12325528fa4dc302415cae56cd68b2455c
Pradeep yadav <pradeep20020102@gmail.com>
2024-12-31 23:44:01 +0545

    docker to run packages tested

---
+++ testing-docker/index.js
@@ -0,0 +1,25 @@
+const {Client } = require('pg');
+
+const connectionString = 'postgresql://postgres:mysecret@localhost:5432/postgres'
+
+const client = new Client({
+    connectionString: connectionString
+});
+
+client.connect(err => {
+    if(err){
+        console.log("connection error", err.stack);
+    } else{
+        console.log("connected to the database")
+    }
+});
+
+client.query("SELECT NOW()",(err,res) => {
+    if(err){
+        console.error(err);
+    }else {
+        console.log(res.rows[0]);
+    }
+
+    client.end();
+})
