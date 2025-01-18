47db2a01d9351a3aafbc62254e96c011c247b660
Pradeep yadav <pradeep20020102@gmail.com>
2025-01-18 23:35:15 +0545

    common package created and exported

---
+++ turborepoClass/packages/common/src/index.ts
@@ -0,0 +1,5 @@
+
+
+export const VALUE = "Pradeep yadav";
+
+export const BACKEND_URL = "https://api.google.com";
---
+++ turborepoClass/packages/ui/src/admin.tsx
@@ -0,0 +1,7 @@
+
+
+export const Admin = ()=>{
+    return <div>
+        <h1>welcome to admin page</h1>
+    </div>
+}
