2d86d0afed4c7d52682339e2beea821552814510
Pradeep yadav <pradeep20020102@gmail.com>
2025-01-31 23:12:49 +0545

    static todo are obtained

---
+++ static-site-generation/app/todos/page.tsx
@@ -0,0 +1,20 @@
+import revalidate from "../lib/actions/action1";
+
+export default async function Home(){
+    const response = await fetch("https://sum-server.100xdevs.com/todos",{
+        next:{
+            tags:['todos']
+        }
+    })
+    const data = await response.json();
+    revalidate()
+
+    return (
+        <div>
+            {data.todos.map((todo:any) => <div key={todo.id}>
+                {todo.title}
+                {todo.description}
+            </div>)}
+        </div>
+    )
+}
